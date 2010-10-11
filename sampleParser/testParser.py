33685f5f92d99fca723dcfe1ce0c02973c652b18
Donald Mcgaughey <dnmcgoy@donald-mcgaugheys-macbook-pro.local>
2010-10-10 22:47:45 -0400

    Implementing single card page parser.

--- sampleParser/testParser.py
+++ sampleParser/testParser.py
@@ -1,5 +1,5 @@
 from HTMLParser import HTMLParser
-    
+
 class MagicParser(HTMLParser):
     cardList = []
     pagingList = []
@@ -7,11 +7,23 @@ class MagicParser(HTMLParser):
     currentTagType = False
     inPagingControl = False
 
+    inSingleCardPage = False
+    singleCardTag = False
+    inValueClass = False
+
+    divCount = 0
+
     def handle_data(self, data):
         if(self.currentTagType):
             if(not self.currentTagType in self.currentCard):
                 self.currentCard[self.currentTagType] = []
             self.currentCard[self.currentTagType].append(data)
+
+        if(self.singleCardTag and self.inValueClass):
+            if(not self.singleCardTag in self.currentCard):
+                self.currentCard[self.singleCardTag] = []
+            self.currentCard[self.singleCardTag].append(data)
+
 
     def handle_starttag(self, tag, attrs):
         evenCardItemAttr = ('class', 'cardItem evenItem')
@@ -47,7 +59,59 @@ class MagicParser(HTMLParser):
         if(tag == "img" and self.currentTagType):
             for attr in attrs:
                 if(attr[0]=="alt"):
+                    if(not self.currentTagType in self.currentCard):
+                        self.currentCard[self.currentTagType] = []
                     self.currentCard[self.currentTagType].append("<symbol>" + attr[1] + "</symbol>")
+
+
+        #this if for the single card parser
+        cardDetailsAttr = ('class', 'cardDetails')
+        classValueAttr = ('class', 'value')
+
+        if(self.singleCardTag and classValueAttr in attrs):
+            self.inValueClass = True
+
+        if(self.singleCardTag and tag == "div"):
+            self.divCount = self.divCount + 1
+
+        if(self.inSingleCardPage):
+            for attr in attrs:
+                if(attr[0] == "id"):
+                    if("nameRow" in attr[1]):
+                        self.singleCardTag = "name"
+                    elif("manaRow" in attr[1]):
+                        self.singleCardTag = "manaCost"
+                    elif("cmcRow" in attr[1]):
+                        self.singleCardTag = "convertedManaCost"
+                    elif("textRow" in attr[1]):
+                        self.singleCardTag = "rules"
+                    elif("typeRow" in attr[1]):
+                        self.singleCardTag = "type"
+                    elif("flavorRow" in attr[1]):
+                        self.singleCardTag = "flavorText"
+                    elif("ptRow" in attr[1]):
+                        self.singleCardTag = "powerToughness"
+                    elif("setRow" in attr[1]):
+                        self.singleCardTag = "set"
+                    elif("rarityRow" in attr[1]):
+                        self.singleCardTag = "rarity"
+                    elif("otherSetsRow" in attr[1]):
+                        self.singleCardTag = "otherSets"
+                    elif("artistRow" in attr[1]):
+                        self.singleCardTag = "artist"
+                 
+        if(cardDetailsAttr in attrs):
+            self.inSingleCardPage = True
+            self.currentCard = dict()
+
+
+        #FIXME copy pasta of the multicard version
+        if(tag == "img" and self.singleCardTag and self.inValueClass):
+            for attr in attrs:
+                if(attr[0]=="alt"):
+                    if(not self.singleCardTag in self.currentCard):
+                        self.currentCard[self.singleCardTag] = []
+                    self.currentCard[self.singleCardTag].append("<symbol>" + attr[1] + "</symbol>")
                     
 
     def handle_endtag(self, tag):
@@ -58,15 +122,25 @@ class MagicParser(HTMLParser):
             self.currentTagType = False
         if(self.inPagingControl and tag == "div"):
             self.inPagingControl = False
+
+        #single card parsing
+        if(tag == "div" and self.inValueClass):
+            self.inValueClass = False
+        if(self.singleCardTag and tag == "div"):
+           if(self.divCount == 0):
+               self.singleCardTag = False
+           else:
+               self.divCount = self.divCount - 1
             
 
 
 import httplib
-
 magicParser = MagicParser()
 conn = httplib.HTTPConnection("gatherer.wizards.com")
 
-f = open("/Users/dnmcgoy/magicTestOutput.txt","w")
+#Test 1
+
+f = open("/Users/dnmcgoy/code/magicTestOutput.txt","w")
 
 lastCardName = "invalid value"
 pageNumber = 0
@@ -89,7 +163,29 @@ while pageNumber < 500:
             f.write("\n")
             f.write("\n")
     pageNumber = pageNumber + 1
-    if (not magicParser.cardList[0] or magicParser.cardList[0]["name"] == lastCardName):
+    if (not magicParser.cardList[0] or 
+        len(magicParser.cardList) == 1 or
+        magicParser.cardList[0]["name"] == lastCardName):
         break
     else:
         lastCardName = magicParser.cardList[0]["name"]
+
+
+#Test 2
+
+magicParser.reset()
+magicParser.cardList = []
+value = "/Pages/Card/Details.aspx?multiverseid=158109"
+conn.request("GET", value)
+magicParser.feed(conn.getresponse().read().decode("UTF-8"))
+card = magicParser.cardList[0]
+if(card):
+    f.write(' '.join(card["name"]).encode("utf-8").strip())
+    f.write("\n")
+    f.write(' '.join(card["manaCost"]).encode("utf-8").strip())
+    f.write("\n")
+    f.write(' '.join(card["type"]).encode("utf-8").strip())
+    f.write("\n")
+    f.write(' '.join(card["rules"]).encode("utf-8").strip())
+    f.write("\n")
+    f.write("\n")
