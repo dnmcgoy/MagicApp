82315c4363fa88322823502e976c7798754e0a40
Donnie McGaughey <savi@donald-mcgaugheys-macbook-pro.local>
2011-05-01 15:28:17 -0400

    Change card size for detail view.

--- titanium/Magic Deck/Resources/Views/CardDetailWindow.js
+++ titanium/Magic Deck/Resources/Views/CardDetailWindow.js
@@ -2,18 +2,13 @@ var CardDetailWindow = {};
 
 (function() {
 
-var cardLabel = Ti.UI.createLabel({
-    		top:300,
-    		text:""
-  		});
-
 var cardImage = Ti.UI.createImageView({
     		defaultImage:"CardBack.jpg",
     		image: "CardBack.jpg",
     		top: 0,
     		left: 0,
-			width:200,
-    		height:300
+		width:250,
+    		height:350
   		});
 
 CardDetailWindow.createDetailWindow = function(cardData) {
@@ -27,10 +22,8 @@ CardDetailWindow.createDetailWindow = function(cardData) {
 	cardImage.image = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" +
            			   cardData.mtg_id +
 	   		 		   "&type=card";
-	cardLabel.text = cardData.rules;
 
 	detailWindow.add(cardImage);
-    detailWindow.add(cardLabel);
 
     return detailWindow;
 };
